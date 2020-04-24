6d332c5517134103a7aed59a7b8c389ae47be133
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-04-23 23:39:25 -0300

    Criando operacao de instanciacao

--- src/main/java/com/mz/lojavirtual/LojaVirtualApplication.java
+++ src/main/java/com/mz/lojavirtual/LojaVirtualApplication.java
@@ -1,13 +1,32 @@
 package com.mz.lojavirtual;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mz.lojavirtual.domain.Categoria;
+import com.mz.lojavirtual.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class LojaVirtualApplication {
+public class LojaVirtualApplication implements CommandLineRunner{
+	
+	@Autowired
+	CategoriaRepository categoriaRepo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LojaVirtualApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepo.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
