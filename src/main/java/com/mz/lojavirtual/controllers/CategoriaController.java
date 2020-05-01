9a6a5b9eb6d95788d687015cdd5412fec7faf237
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-04-30 23:07:58 -0300

    Listando todas categorias com DTO

--- src/main/java/com/mz/lojavirtual/controllers/CategoriaController.java
+++ src/main/java/com/mz/lojavirtual/controllers/CategoriaController.java
@@ -1,6 +1,8 @@
 package com.mz.lojavirtual.controllers;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mz.lojavirtual.domain.Categoria;
+import com.mz.lojavirtual.dto.CategoriaDTO;
 import com.mz.lojavirtual.services.CategoriaService;
 
 @RestController
@@ -46,5 +49,12 @@ public class CategoriaController {
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
-	}	
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> categorias = categoriaService.findAll();
+		List<CategoriaDTO> categoriasDTO = categorias.stream().map(c -> new CategoriaDTO(c)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(categoriasDTO);
+	}
 }
