7131afd13f007e2cff809f0097b2a2222721ed70
Edilson Moizinho <edilsondiasalves@outlook.com>
2020-05-15 02:11:37 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/mz/lojavirtual/config/DevConfig.java
+++ src/main/java/com/mz/lojavirtual/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.mz.lojavirtual.services.DBService;
+import com.mz.lojavirtual.services.EmailService;
+import com.mz.lojavirtual.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		
 		dbService.initializeLocalDatabase();
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
